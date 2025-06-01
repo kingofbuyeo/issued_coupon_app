f4349f57596b7e341332bba26959c6924b704e4c
lastpice <lastpice@gmail.com>
2025-06-01 20:45:53 +0900

    에러 핸들링추가

--- src/main/java/yong/chul/coupon/issued_coupon_app/infrastructure/ApiExceptionController.kt
+++ src/main/java/yong/chul/coupon/issued_coupon_app/infrastructure/ApiExceptionController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import yong.chul.coupon.issued_coupon_app.usecase.exception.CouponNotAvailableIssuedException
 import yong.chul.coupon.issued_coupon_app.usecase.exception.DuplicateCouponException
 import yong.chul.coupon.issued_coupon_app.usecase.exception.OverIssuedCouponException
 
@@ -56,5 +57,11 @@ class ApiExceptionController {
     @ExceptionHandler(value = [DuplicateCouponException::class])
     fun duplicateCouponException(e: DuplicateCouponException): ErrorRes {
         return ErrorRes(406011, e.message)
+    }
+
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    @ExceptionHandler(value = [CouponNotAvailableIssuedException::class])
+    fun couponNotAvailableExceptionHandler(e: CouponNotAvailableIssuedException): ErrorRes {
+        return ErrorRes(406021, e.message)
     }
 }
